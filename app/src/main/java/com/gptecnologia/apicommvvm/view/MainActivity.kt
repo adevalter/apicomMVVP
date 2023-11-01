ed7378b72bfbd57106efbafae1860e86e72fc384
Adevalter <adevalter@gmail.com>
2023-11-01 06:48:15 -0300

    implementando viewaciivy para exibir os dados

--- app/src/main/java/com/gptecnologia/apicommvvm/view/MainActivity.kt
+++ app/src/main/java/com/gptecnologia/apicommvvm/view/MainActivity.kt
@@ -2,8 +2,11 @@ package com.gptecnologia.apicommvvm.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.gptecnologia.apicommvvm.R
 import com.gptecnologia.apicommvvm.databinding.ActivityMainBinding
+import com.gptecnologia.apicommvvm.viewmodel.PostagemViewModel
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,8 +14,26 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate( layoutInflater)
     }
 
+    private lateinit var  mainViewModel: PostagemViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        mainViewModel = ViewModelProvider(this).get(PostagemViewModel::class.java)
+        mainViewModel.listaPostagens.observe(this){listaPostagens ->
+            var listaResultado = ""
+            listaPostagens.forEach {postagem ->
+                listaResultado = " (${postagem.id}) ${postagem.title} \n"
+
+            }
+            binding.textResultado.text = listaResultado
+
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mainViewModel.recuperarPostagens()
     }
 }
